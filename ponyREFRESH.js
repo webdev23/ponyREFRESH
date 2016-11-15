2f58afb4c2b0c20d3b6a6b028d1d07be03e1ad3b
Nico KraZhtest <webdev23@users.noreply.github.com>
2016-11-15 16:36:09 +0100

    Now works everywhere

--- ponyREFRESH.js
+++ ponyREFRESH.js
@@ -14,13 +14,13 @@ src="https://raw.githubusercontent.com/webdev23/ponyREFRESH/master/ponyREFRESH.j
 
 let rEFRESH
 ,in_SECONDS=document.querySelectorAll('[data-seconds]')[0].dataset.seconds
-	,pxTOP=document.querySelectorAll('[data-px]')[0].dataset.px
+	,px=document.querySelectorAll('[data-px]')[0].dataset.px
 	,sCROLL=document.querySelectorAll('[data-scroll]')[0].dataset.scroll
 	,aCTIVE=document.querySelectorAll('[data-active]')[0].dataset.active
 function pwnIN(){window.clearTimeout(rEFRESH)}
-function pwnOUT(){if(sCROLL==true){window.scrollTo(0,pxTOP)};
+function pwnOUT(){if(sCROLL==true){window.scrollTo(0,px)};
 if(aCTIVE==true){rEFRESH=window.setTimeout(reload,(in_SECONDS)*1000)}}
 function reload(){window.location.reload(true)}
-document.body.setAttribute('onmouseleave',pwnOUT())
-document.body.setAttribute('onmouseenter',pwnIN())
+document.body.setAttribute('onmouseleave','pwnOUT()')
+document.body.setAttribute('onmouseenter','pwnIN()')
 pwnOUT()
